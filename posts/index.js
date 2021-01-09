c7905d84ab1f7c801507607834d1b8b3eda50f2f
Prakash Sharma <prakashsharma91@gmail.com>
2021-01-09 15:02:28 +0530

    comment moderation added

--- posts/index.js
+++ posts/index.js
@@ -40,5 +40,5 @@ app.post('/events', (req, res) => {
 })
 
 app.listen(4000, () => {
-    console.log("Listening on 4000")
+    console.log("Listening on 4000 : posts-service")
 })
--- query/index.js
+++ query/index.js
@@ -22,15 +22,29 @@ app.post('/events', (req, res) => {
     }
 
     if (type === 'CommentCreated') {
-        const { id, content, postId } = data;
+        const { id, content, postId, status } = data;
 
         const post = posts[postId];
-        post.comments.push({ id, content })
+        post.comments.push({ id, content, status })
+    }
+
+
+    if (type === 'CommentUpdated') {
+        const { id, content, postId, status } = data;
+
+        const post = posts[postId];
+
+        const comment = post.comments.find(comment => {
+            return comment.id === id;
+        })
+
+        comment.status = status
+        comment.content = content
     }
 
     res.send({});
 })
 
 app.listen(4002, () => {
-    console.log("Listing on port", 4002)
+    console.log("Listing on port 4002 : query-service")
 })
